783962295cb17ce50611fb50b8c5128f613e81f9
Pete Considine <pete.considine@gmail.com>
2016-10-04 13:38:02 -0700

    Fixing mixed active content errors

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 // Email obfuscator script 2.1 by Tim Williams, University of Arizona. Random encryption key feature by Andrew Moulden, Site Engineering Ltd. This code is freeware provided these four comment lines remain intact. A wizard to generate this code is at http://www.jottings.com/obfuscator/
 
-{
-  coded = "grZr.1BDiwFwDr@QCAwO.1BC"
-  key = "ob0EjLADW4RsJpuMlxXhTN7vBIdVm5etc32i9gqKOH6Ganz8kfFYCSrZ1PUwyQ"
-  shift = coded.length
-  link = ""
-  for (i = 0; i < coded.length; i++) {
+$(function emailObscurer(){
+  var coded = "grZr.1BDiwFwDr@QCAwO.1BC"
+  var key = "ob0EjLADW4RsJpuMlxXhTN7vBIdVm5etc32i9gqKOH6Ganz8kfFYCSrZ1PUwyQ"
+  var shift = coded.length
+  var link = ""
+  for (var i = 0; i < coded.length; i++) {
     if (key.indexOf(coded.charAt(i)) == -1) {
-      ltr = coded.charAt(i)
+      var ltr = coded.charAt(i)
       link += (ltr)
     } else {
-      ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
+      var ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
       link += (key.charAt(ltr))
     }
   }
-document.getElementById('email').innerHTML=("<a target='_blank' href='mailto:" + link + "'>Email</a>")
-}
+  $("#email").html("<a href='mailto:" + link + "'>Email</a>")
+});
 
 var url, lat, lon, city, state;
 
 $(document).ready(function() {
   $.ajax({
     type: 'GET',
-    url: 'http://ip-api.com/json',
+    url: 'https://ip-api.com/json',
     async: true,
     contentType: 'application/json',
     dataType: 'jsonp',
